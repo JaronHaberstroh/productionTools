3bb093eaae25511ae27b2ee567aca2100bc8f313
JHSpaz988 <JHSpaz988@gmail.com>
2023-10-30 15:49:57 -0400

    refactoring and completed routes for Employee crud

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -3,7 +3,9 @@ import * as employeeController from "../controllers/employeeController.js";
 
 const router = express.Router();
 
-router.get("/", employeeController.getAll);
+router.get("/find", employeeController.getEmployeesList);
 router.post("/create", employeeController.addEmployee);
+router.put("/edit", employeeController.editEmployee);
+router.delete("/delete", employeeController.deleteEmployee);
 
 export default router;
